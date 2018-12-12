b047bc7ebafad1c01b9d2e05a4daad138b6db1a4
Fairiz Azizi <coderfi@gmail.com>
2018-12-12 11:31:46 -0800

    add simple AppBar to header

--- src/pages/Home/header/Header.js
+++ src/pages/Home/header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import PropTypes from 'prop-types';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-const Header = () => {
+const styles = () => ({
+  appBar: {
+    position: 'relative',
+  },
+});
+
+const Header = (props) => {
+  const { classes } = props;
+
   return (
     <div>
+      <AppBar color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            React Starter
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Typography component="h1" variant="h4" align="center" color="textPrimary" gutterBottom>
         Header
       </Typography>
@@ -15,4 +34,8 @@ const Header = () => {
   );
 };
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
